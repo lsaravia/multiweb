2b8078ee3370bbb1ced3d8a63974665f1ddff879
Leonardo Saravia <lsaravia@ungs.edu.ar>
2018-07-25 10:58:10 -0300

    Updated functions and documentation

--- R/functions.r
+++ R/functions.r
@@ -14,22 +14,22 @@
 #'
 #' # Reads a network in edge list (interaction list) format, with predators as the first column
 #' #
-#' fileName <- system.file("extdata", "WeddellSea_FW.csv", package = "EcoNetwork"))
-#' g <- readEcoNetwork(fileName)
+#' fileName <- system.file("extdata", "WeddellSea_FW.csv", package = "EcoNetwork")
+#' g <- readNetwork(fileName)
 #'
 #' # Reads a network in adyacency matrix format, with predators as columns
 #' #
-#' fileName <- system.file("extdata", "BarentsBoreal_FW.csv", package = "EcoNetwork"))
-#' g <- readEcoNetwork(fileName)
+#' fileName <- system.file("extdata", "BarentsBoreal_FW.csv", package = "EcoNetwork")
+#' g <- readNetwork(fileName)
 #'
 #' # Read a vector of files
 #' #
 #'\dontrun{
 #' dn <- list.files("inst/extdata",pattern = "^.*\\.csv$")
-#' netData <- readEcoNetwork(dn,"inst/extdata")
+#' netData <- readNetwork(dn,"inst/extdata")
 #'}
 
-readEcoNetwork <- function(fileName,filePath=NULL){
+readNetwork <- function(fileName,filePath=NULL){
 
   fn <-  if(!is.null(filePath)) paste0(filePath,"/",fileName) else fileName
 
@@ -73,18 +73,19 @@ readEcoNetwork <- function(fileName,filePath=NULL){
 #' @param ig igraph object
 #' @param vertexLabel logical plot vertex labels
 #' @param vertexSizeFactor numeric factor to determine the size of the label with degree
+#' @param tk TRUE generate an interactive plot using tkplot
 #'
 #' @return a plot
 #' @export
 #'
 #'
 #' @importFrom NetIndices TrophInd
-#' @importFrom igraph     V degree
+#' @importFrom igraph     V degree get.adjacency
 #' @importFrom RColorBrewer brewer.pal
 #' @examples
 #'
-#' plotEcoNetworkTrophLevel(netData[[1]])
-plotEcoNetworkTrophLevel <- function(g,vertexLabel=FALSE,vertexSizeFactor=5){
+#' plotTrophLevel(netData[[1]])
+plotTrophLevel <- function(g,vertexLabel=FALSE,vertexSizeFactor=5,tk=FALSE){
 
   deg <- degree(g, mode="all") # calculate the degree: the number of edges
   # or interactions
@@ -98,7 +99,7 @@ plotEcoNetworkTrophLevel <- function(g,vertexLabel=FALSE,vertexSizeFactor=5){
   if(!vertexLabel)
     V(g)$label <- NA
 
-  tl <- NetIndices::TrophInd(get.adjacency(g,sparse=F))  # Calculate the trophic level
+  tl <- TrophInd(get.adjacency(g,sparse=F))  # Calculate the trophic level
 
   # Layout matrix to specify the position of each vertix
   # Rows equal to the number of vertices (species)
@@ -115,11 +116,20 @@ plotEcoNetworkTrophLevel <- function(g,vertexLabel=FALSE,vertexSizeFactor=5){
   V(g)$color <- colnet[12-colTL]      # Add colors to the igraph object
 
 
-  plot(g, edge.width=.3,edge.arrow.size=.4,
-       #vertex.label=NA,
+  if(tk){
+    tkid <- tkplot(g, edge.width=.3,edge.arrow.size=.4,
        vertex.label.color="white",
        edge.color="grey50",
        edge.curved=0.3, layout=lMat)
+    return( tkplot.getcoords(tkid))
+
+  } else {
+    plot(g, edge.width=.3,edge.arrow.size=.4,
+         vertex.label.color="white",
+         edge.color="grey50",
+         edge.curved=0.3, layout=lMat)
+
+  }
 
 
 }
@@ -131,20 +141,42 @@ plotEcoNetworkTrophLevel <- function(g,vertexLabel=FALSE,vertexSizeFactor=5){
 #' @param ig vector of igraph objects
 #'
 #' @return a data.frame with the following fields:
-#'         Size: Number of species
-#'         Top:  Number of top predator species
-#'         Basal: Number of basal especies
-#'         Links: number of interactions
-#'         LD: linkage density
-#'         Connectance: Connectance
-#'         PathLength: average path length
-#'         Clustering: clustering coeficient
-#'         Cannib: number of cannibalistic species
+#'
+#'         \item{Size:}{Number of species}
+#'         \item{Top:}{Number of top predator species}
+#'         \item{Basal:}{Number of basal especies}
+#'         \item{Omnivory:}{ Number of omnivory species, is the number }
+#'         \item{Links:}{ number of interactions}
+#'         \item{LD:}{ linkage density}
+#'         \item{Connectance:}{ Connectance}
+#'         \item{PathLength:}{ average path length}
+#'         \item{Clustering:}{ clustering coeficient}
+#'         \item{Cannib:}{ number of cannibalistic species}
+#'         \item{TLmean:}{ mean trophic level}
+#'         \item{Cannib:}{ maximum trophic level}
+#'
 #'
 #' @export
+#' @importFrom NetIndices TrophInd
+#' @import igraph
 #'
 #' @examples
-topologicalIndicesEcoNetwork <- function(ig){
+#'
+#' topologicalIndices(netData)
+#'
+#' # Generate a test network
+#'
+#' g <-   graph_from_literal( 1 -+ 4 -+ 7,2 -+ 5 -+7, 3-+6-+7, 7-+7, 4+-3, simplify = FALSE)
+#'
+#' topologicalIndices(g)
+
+topologicalIndices <- function(ig){
+
+  if(class(ig)=="igraph") {
+    ig <- list(ig)
+  } else if(class(ig[[1]])!="igraph") {
+    stop("parameter ig must be an igraph object")
+  }
 
   df <- lapply(ig, function(g){
 
@@ -176,14 +208,29 @@ topologicalIndicesEcoNetwork <- function(ig){
 
     cannib <- sum(which_loop(g))
 
-    data.frame(Size=size,Top=nTop,Basal=nBasal,Links=links, LD=linkDen,Connectance=conn,PathLength=pathLength,Clustering=clusCoef, Cannib=cannib)
+    tl <- TrophInd(get.adjacency(g,sparse=F))  # Calculate the trophic level
+
+    omn <- sum(round(tl$OI,5)>0)
+
+    data.frame(Size=size,Top=nTop,Basal=nBasal,Omnivory=omn,Links=links, LD=linkDen,Connectance=conn,PathLength=pathLength,
+      Clustering=clusCoef, Cannib=cannib, TLmean=mean(tl$TL),TLmax=max(tl$TL))
   })
 
   do.call(rbind,df)
 }
 
 
-parTopologicalIndicesEcoNetwork <- function(ig){
+#' Paralell version of topologicalIndices
+#'
+#' @param ig vector of igraph objects
+#'
+#' @return the same as topologicalIndices
+#' @export
+#'
+#' @examples
+#'
+#' parTopologicalIndices(netData)
+parTopologicalIndices <- function(ig){
 
   cn <-detectCores()
   cl <- makeCluster(cn)
@@ -191,39 +238,64 @@ parTopologicalIndicesEcoNetwork <- function(ig){
 
   df <- foreach(i=seq_along(ig), .combine='rbind',.inorder=FALSE,.packages='igraph') %dopar% {
 
-    g <- ig[[i]]
+    topologicalIndices(g)
 
-    deg <- degree(simplify(g), mode="out") # calculate the out-degree: the number of predators
-
-    V(g)$outdegree <-  deg
-
-    nTop <- length(V(g)[outdegree==0]) # Top predators do not have predators
-
-    deg <- degree(g, mode="in") # calculate the in-degree: the number of preys
-
-    V(g)$indegree <-  deg
-
-    nBasal <- length(V(g)[indegree==0]) # Basal species do not have preys
-
-    vcount(g)-nTop-nBasal
-
-    size <- vcount(g)
-
-    links <- ecount(g)
-
-    linkDen <- links/size          # Linkage density
-
-    conn <- links/size^2           # Connectance
-
-    pathLength <- average.path.length(g)   # Average path length
-
-    clusCoef <- transitivity(g, type = "global")
-
-    cannib <- sum(which_loop(g))
-
-    data.frame(Size=size,Top=nTop,Basal=nBasal,Links=links, LD=linkDen,Connectance=conn,PathLength=pathLength,Clustering=clusCoef, Cannib=cannib)
   }
   stopCluster(cl)
 
   return(df)
+}
+
+
+#' Calculate the incoherence index of a food web
+#'
+#' The incoherence index is based in how the species fit in discrete trophic levels
+#' when Q is closer to 0 more coherent and stable is a food web.
+#'
+#' Based on:
+#'
+#' @references Johnson, S., Domínguez-García, V., Donetti, L., & Muñoz, M. A. (2014). Trophic coherence determines food-web stability. Proceedings of the National Academy of Sciences , 111(50), 17923–17928. https://doi.org/10.1073/pnas.1409077111
+#'
+#' @references Johnson, S., & Jones, N. S. (2017). Looplessness in networks is linked to trophic coherence. Proceedings of the National Academy of Sciences, 114(22), 5618–5623. https://doi.org/10.1073/pnas.1613786114
+#'
+#'
+#' @param g an igraph object
+#' @param ti trophic level vector if not supplied is calculated internally
+#'
+#' @return a data.frame with the following fields
+#'
+#'       \item{Q}{incoherence (0=coherent)}
+#'      \item{rQ}{ratio of Q with expected Q under null expectation given N=nodes L=links B=basal nodes Lb=basal links}
+#'      \item{mTi}{mean trophic level}
+#'      \item{rTI}{ration of TI with expected TI under the same null}
+#
+#'
+#' @export
+#'
+#' @examples
+#'
+#' calcIncoherence(netData[[1]])
+#'
+#'
+calcIncoherence <- function(g,ti=NULL) {
+  require(igraph)
+  require(NetIndices)
+  if(is.null(ti) )
+    ti<-TrophInd(get.adjacency(g,sparse=FALSE))
+  v <- ti$TL
+  z <- outer(v,v,'-');
+  A <- get.adjacency(g,sparse = FALSE)
+  xx <- A>0
+  x <- (A*t(z))[xx]
+  #meanQ <- sum(x)/ecount(g)
+  #sdQ <- sqrt(sum((x-1)^2)/vcount(g) )
+  Q <- sqrt(sum(x*x-1)/ecount(g) )
+
+  basal <- which(round(v,8)==1)
+  bedges <- sum(degree(g,basal,mode='out'))
+  mTI <- mean(v)
+  mK <- mean(degree(g,mode='out'))
+  eTI <- 1+(1-length(basal)/vcount(g))*ecount(g)/bedges
+  eQ <- sqrt(ecount(g)/bedges-1)
+  data.frame(Q=Q,rQ=Q/eQ,mTI=mTI,rTI=mTI/eTI)
 }
