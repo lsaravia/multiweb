a946ab28acb83389cc921c85c41af3e11d616f14
Leonardo Saravia <lsaravia@ungs.edu.ar>
2019-07-17 09:56:26 -0300

    Add missing files to change name

---
+++ R/data.r
@@ -0,0 +1,12 @@
+#' Compilation of Ecological Networks (food webs) in igraph format.
+#'
+#' A dataset 23 marine ecological networks (food webs) from [1]
+#'
+#' @format A list with 23 igraph network objects
+#'
+#' @source \url{https://doi.org/10.1371/journal.pone.0198217}
+#'
+#' @references
+#'
+#' [1] Marina, T. I. et al. 2018. Architecture of marine food webs: To be or not be a ‘small-world.’ - PLoS ONE 13: 1–13.
+"netData"
